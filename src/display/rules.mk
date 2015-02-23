1cf88df72f105eb9dc82ae224a13f612648be0dc
urbolabs <urbolabs@gmail.com>
2015-02-23 11:21:36 -0600

    Add display-mux macro module

--- src/display/rules.mk
+++ src/display/rules.mk
@@ -18,3 +18,14 @@ ${file}.fst: SIM_REG += --register ${PORTB}
 ${file}.fst: SIM_REG += --register ${PORTD}
 AVR_HEX += ${file}.hex
 AVR_FST += ${file}.fst
+
+
+file := $d/display-mux.avr
+${file}.hex: AVRA_LIB := $d
+${file}.hex: ${file}.inc
+${file}.fst: $f
+${file}.fst: SIM_CYCLES := 3000
+${file}.fst: SIM_REG += --register ${PORTB}
+${file}.fst: SIM_REG += --register ${PORTD}
+AVR_HEX += ${file}.hex
+AVR_FST += ${file}.fst
